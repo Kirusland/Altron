5622e42ef010d3ce407c72c84baec52b549f9078
Kirill <73121729+kirillmatytsyn@users.noreply.github.com>
2023-03-12 19:14:56 +0300

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,32 @@
+import pyaudio
+import speech_recognition as sr
+import random
+
+
+
+r = sr.Recognizer()
+greetings = ["Здравствуйте!","Приветик!", "Привет, пупсик!", "Привет, как дела?"]
+films = ["Титаник","Смешарики","Джуманджи","Я чурка"]
+while True:
+    with sr.Microphone(device_index=1) as source: 
+        print("Скажите что-нибудь ...")
+        audio = r.listen(source)
+    try:
+        speech = r.recognize_google(audio, language="ru_RU").lower()
+        print("Вы сказали: " + speech)
+
+        if "привет" in speech:  
+            print(random.choice(greetings))
+        elif "фильм" in speech:  
+                print("Посмотрите "+random.choice(films))
+        elif "открой файл" in speech:
+            os.startfile("C:/Users/Ученик/Downloads/inecraft_death.wav")
+        elif "youtube" in speech:
+            webbrowser.open_new("https://www.youtube.com/?gl=RU&hl=ru")
+        elif speech == "пока" in speech:  
+            print("До свидания")
+            break
+        else:
+            print("Я вас не понимаю, повторите пожалуйста")
+    except:
+        print("Ошибка")
